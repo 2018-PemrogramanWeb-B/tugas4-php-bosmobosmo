4b0247625153a3fa661b4b6e5b8c03b7c3de4393
bosmobosmo <36439009+bosmobosmo@users.noreply.github.com>
2018-10-01 19:21:36 +0700

    form validation

--- index.php
+++ index.php
@@ -3,7 +3,24 @@
     <title>Login</title>
   </head>
   <body>
-    <form action="welcome.php" method="post">
+
+    <?php
+    $username = $password = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $username = test_input($_POST["username"]);
+      $password = test_input($_POST["password"]);
+    }
+
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
       Username : <input type="text" name="username"><br>
       Password : <input type="password" name="password"><br>
       <input type="submit">
