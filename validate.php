c1873b16c969ea59c6e07ea8da41573a471eeb5a
bosmobosmo <36439009+bosmobosmo@users.noreply.github.com>
2018-10-02 10:27:27 +0700

    move welcome to validate

--- validate.php
+++ validate.php
@@ -6,7 +6,23 @@ session_start();
       $_SESSION['valid'] = true;
       $_SESSION['username'] = 'user';
 
-      header('location:welcome.php');
+      // header('location:welcome.php');
+      date_default_timezone_set('Asia/Jakarta');
+         $time = date("H");
+         // echo $time;
+         // $timezone = date("e");
+         if ($time < "12") {
+             echo "Selamat Pagi!!";
+         } else
+         if ($time >= "12" && $time < "17") {
+             echo "Selamat Siang!!";
+         } else
+         if ($time >= "17" && $time < "19") {
+             echo "Selamat Sore!!";
+         } else
+         if ($time >= "19") {
+             echo "Selamat Malam!!";
+         }
    }else {
      header('location:index.php');
    }
